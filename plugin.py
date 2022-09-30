e1e09daf507adc72109a73879f27e87a5e462286
Addie Janssen <me@addiejanssen.com>
2022-09-30 18:13:02 +0200

    Fix inverter friendly name

--- plugin.py
+++ plugin.py
@@ -364,7 +364,7 @@ class BasePlugin:
                         Domoticz.Log("Inverter returned information")
 
                         inverter_type = solaredge_modbus.sunspecDID(inverter_values["c_sunspec_did"])
-                        Domoticz.Log("Inverter type: {}".format(solaredge_modbus.C_SUNSPEC_DID_MAP[inverter_type]))
+                        Domoticz.Log("Inverter type: {}".format(solaredge_modbus.C_SUNSPEC_DID_MAP[inverter_values["c_sunspec_did"]]))
 
                         device_offset = 0
                         details = {
