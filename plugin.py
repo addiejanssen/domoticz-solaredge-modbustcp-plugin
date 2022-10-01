c2036051d89b6dfad5f978efeb7bdb80a43ad32b
Addie Janssen <me@addiejanssen.com>
2022-10-01 14:52:20 +0200

    updated plugin version

--- plugin.py
+++ plugin.py
@@ -9,7 +9,7 @@
 #
 
 """
-<plugin key="SolarEdge_ModbusTCP" name="SolarEdge ModbusTCP" author="Addie Janssen" version="2.0.0" externallink="https://github.com/addiejanssen/domoticz-solaredge-modbustcp-plugin">
+<plugin key="SolarEdge_ModbusTCP" name="SolarEdge ModbusTCP" author="Addie Janssen" version="2.0.1" externallink="https://github.com/addiejanssen/domoticz-solaredge-modbustcp-plugin">
     <params>
         <param field="Address" label="Inverter IP Address" width="150px" required="true" />
         <param field="Port" label="Inverter Port Number" width="100px" required="true" default="502" />
@@ -141,7 +141,7 @@ class BasePlugin:
 
         # Set the logging level
 
-        Domoticz.Log("mode 5 = {}".format(Parameters["Mode5"].value))
+        Domoticz.Log("mode 5 = {}".format(Parameters["Mode5"]))
 
         SetLogLevel(LogLevels(Parameters["Mode5"]))
 
