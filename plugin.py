334e340f7c2269308889a806877c032deb80f4e8
Addie Janssen <me@addiejanssen.com>
2022-10-29 15:56:09 +0200

    Fixed logging

--- plugin.py
+++ plugin.py
@@ -422,7 +422,7 @@ class BasePlugin:
                         to_log = inverter_values
                         if "c_serialnumber" in to_log:
                             to_log.pop("c_serialnumber")
-                        DomoLog(LogLevels.VERBOSE, "device: {} values: {}".format(device_name, json.dumps(to_log, indent=4, sort_keys=False)))
+                        DomoLog(LogLevels.VERBOSE, "values: {}".format(json.dumps(to_log, indent=4, sort_keys=False)))
 
                         inverter_type = solaredge_modbus.sunspecDID(inverter_values["c_sunspec_did"])
                         DomoLog(LogLevels.NORMAL, "Inverter type: {}".format(solaredge_modbus.C_SUNSPEC_DID_MAP[str(inverter_type.value)]))
